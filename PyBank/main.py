3a1a4d0eed3e98fb6997b6730a456a9e990f4c86
rashaad18 <70591320+rashaad18@users.noreply.github.com>
2020-09-29 20:04:31 -0400

    Added output to text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,22 +3,62 @@
 import os
 import csv
 
+rowCount = 0
+totalProfitLoss = 0
+average = 0
+greatest_temp = {
+    "month" : "",
+    "money" : 0
+    }
+decreased_temp = {
+    "month" : "",
+    "money" : 0
+    }
+
 # Set the path for csv file
 csv_path = os.path.join("..", "Resources","budget_data.csv")
 
 #open csv file
 with open(csv_path) as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
-    rowCount = 0
+
+    # iterates through the dataset
     for row in csv_reader:
-        print(row[0] + "   " + row[1])
+       # print(row[0] + "   " + row[1])
+        if(row[1] != "Profit/Losses"):
+            totalProfitLoss = totalProfitLoss + int(row[1])
+            if(int(row[1]) > int(greatest_temp["money"])):
+                greatest_temp["month"] = row[0]
+                greatest_temp["money"] = int(row[1])
+            if(int(row[1]) < int(decreased_temp["money"])):
+                decreased_temp["month"] = row[0]
+                decreased_temp["money"] = int(row[1])
+        
+        # gets the number of rows in the dataset
         rowCount = rowCount + 1
+    
+    #subtracts the header row from the count to get the number of rows in the dataset
     rowCount = rowCount - 1
+   
+    # gets the average of the dataset
+    average = int(totalProfitLoss/rowCount)
+
+file_write_path = os.path.join("..", "Analysis","budget_data_finshed_final.txt")
+with open(file_write_path, "a") as data_file:
+    data_file.write("Financial Analysis\n")
+    data_file.write("-----------------------\n")
+    data_file.write("Total Months : " + str(rowCount) + "\n")
+    data_file.write("Total : $" + str(totalProfitLoss) + "\n")
+    data_file.write("Average  Change: " + str(average) + "\n")
+    data_file.write("Greatest Increase in Profits: " + str(greatest_temp["month"]) + " ($" + str(greatest_temp["money"]) + ")\n" )
+    data_file.write("Greatest Decrease in Profits: "  + str(decreased_temp["month"]) + " ($" + str(decreased_temp["money"]) + ")\n")
+    data_file.close
+
 # print output to console with all analysis data
-print("Financial Analysis")
+print("\nFinancial Analysis")
 print("-----------------------")
 print("Total Months : " + str(rowCount))
-print("Total : $")
-print("Average  Change: ")
-print("Greatest Increase in Profits: ")
-print("Greatest Decrease in Profits: ")
+print("Total : $" + str(totalProfitLoss))
+print("Average  Change: " + str(average))
+print("Greatest Increase in Profits: " + str(greatest_temp["month"]) + " ($" + str(greatest_temp["money"]) + ")" )
+print("Greatest Decrease in Profits: "  + str(decreased_temp["month"]) + " ($" + str(decreased_temp["money"]) + ")\n")
